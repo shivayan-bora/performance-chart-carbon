7114b6b8b227507f7b9169ccb4057832d78243b3
Shivayan Bora <bora.shivayan@gmail.com>
2024-07-01 10:27:49 +0530

    firebase deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ "projectId": "fir-angular-88a89", "appId": "1:310251604689:web:6c8a3b62cdf0ee5bd813f0", "storageBucket": "fir-angular-88a89.appspot.com", "apiKey": "AIzaSyAaH8wJR_xZnyFmxoTfywZ0ORC-YdVm6BM", "authDomain": "fir-angular-88a89.firebaseapp.com", "messagingSenderId": "310251604689" })), provideAppCheck(() => {
+    // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+    const provider = new ReCaptchaEnterpriseProvider("6LfwBQUqAAAAAFVvUxrzuebBqVJy8BGlC3PXnIQR");
+    return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+  })]
 };
